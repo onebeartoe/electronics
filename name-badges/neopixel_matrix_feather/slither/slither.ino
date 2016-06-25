25ac197028fba5b94037eb81dc81e38208175d97
Roberto Marquez <onebeartoe@gmail.com>
2016-06-25 00:08:15 -0500

    Yay! The worm stays on the screen.

--- name-badges/neopixel_matrix_feather/slither/slither.ino
+++ name-badges/neopixel_matrix_feather/slither/slither.ino
@@ -1,3 +1,4 @@
+
 /**
  * This display is a take on the work game slither.io
  * 
@@ -37,13 +38,15 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 
+const int width = 8;
+const int height = 4;
 
 // Example for NeoPixel Shield.  In this application we'd like to use it
 // as a 5x8 tall matrix, with the USB port positioned at the top of the
 // Arduino.  When held that way, the first pixel is at the top right, and
 // lines are arranged in columns, progressive order.  The shield uses
 // 800 KHz (v2) pixels that expect GRB color data.
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 4, PIN,
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(width, height, PIN,
   NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
   NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
   NEO_GRB            + NEO_KHZ800);
@@ -53,17 +56,25 @@ const uint16_t colors[] =
   matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) 
 };
 
-int wormLength = 3;
+int wormLength = 4;
 
 /**
  * the element at snake1[0][0] is the head of the worm
  */
-int worm [3] [2] = 
+int worm [4] [2] = 
 {
+  {3, 0},
   {2, 0},
   {1, 0},
   {0, 0}
 };
+
+int validMovesCount;
+
+/**
+ * This two dimentional array holds the x,y coordinates for a total of 4 possible move locations; up, down, left, and right.
+ */
+volatile int validMoves [4][2];
 
 void drawWorm()
 {
@@ -80,20 +91,23 @@ void drawWorm()
 void loop() 
 {
   matrix.fillScreen(0);
-  
+
+/*  
   matrix.drawPixel(1,1, colors[1]);
   matrix.drawPixel(0,0, colors[1]);
 
   matrix.drawPixel(3,3, colors[0]);
   matrix.drawPixel(4,3, colors[0]);
   matrix.drawPixel(2,3, colors[0]);
+*/
 
   moveWorm();
+  
   drawWorm();
   
   matrix.show();
   
-  delay(1500);
+  delay(500);
 }
 
 void moveWorm()
@@ -105,7 +119,15 @@ void moveWorm()
         worm[i][1] = worm[i-1][1];
     }
 
-    worm[0][0] += 1;
+    updateValidMoves();
+
+    int m = random(0, validMovesCount);
+
+    int x = validMoves[m][0];
+    int y = validMoves[m][1];
+
+    worm[0][0] = x;
+    worm[0][1] = y;
 }
 
 void setup() 
@@ -113,5 +135,51 @@ void setup()
   matrix.begin();
 
   matrix.setBrightness(5);
+}
+
+void updateValidMoves()
+{
+    int headX = worm[0][0];
+    int headY = worm[0][1];
+
+    int i = 0;
+
+    // up
+    if(headY > 0)
+    {
+        validMoves[i][1] = headY - 1;
+        validMoves[i][0] = headX;
+        
+        i++;
+    }
+
+    // down
+    if(headY < height-1)
+    {
+        validMoves[i][1] = headY + 1;
+        validMoves[i][0] = headX;
+
+        i++;
+    }
+    
+    // left
+    if(headX > 0)
+    {
+        validMoves[i][0] = headX - 1;
+        validMoves[i][1] = headY;
+
+        i++;
+    }
+
+    // right
+    if(headX < width-1)
+    {
+        validMoves[i][0] = headX + 1;
+        validMoves[i][1] = headY;
+
+        i++;
+    }
+
+    validMovesCount = i;
 }
 
