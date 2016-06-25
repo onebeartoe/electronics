fa833c9447a19c48df13b0a735c365e188823525
Roberto Marquez <onebeartoe@gmail.com>
2016-06-24 20:27:24 -0500

    This is a move refactor.

--- name-badges/neopixel_matrix_feather_strand_test/neopixel_matrix_feather_strand_test.ino.ino
+++ name-badges/neopixel_matrix_feather_strand_test/neopixel_matrix_feather_strand_test.ino.ino
@@ -42,7 +42,8 @@ void loop()
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) 
 {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) 
+  {
       strip.setPixelColor(i, c);
       strip.show();
       delay(wait);
