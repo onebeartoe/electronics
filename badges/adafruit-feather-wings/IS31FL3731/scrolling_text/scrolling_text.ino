0baa0db82ab3e1dc54edfa70af11fbccac42dfd2
onebeartoe <onebeartoe@gmail.com>
2016-07-20 11:24:32 -0500

    The cursor was moved to the origin.

--- badges/adafruit-feather-wings/IS31FL3731/scrolling_text/scrolling_text.ino
+++ badges/adafruit-feather-wings/IS31FL3731/scrolling_text/scrolling_text.ino
@@ -17,56 +17,27 @@
 
 Adafruit_IS31FL3731_Wing matrix = Adafruit_IS31FL3731_Wing();
 
-/*
-static const uint8_t PROGMEM
-  smile_bmp[] =
-  { B00111100,
-    B01000010,
-    B10100101,
-    B10000001,
-    B10100101,
-    B10011001,
-    B01000010,
-    B00111100 },
-  neutral_bmp[] =
-  { B00111100,
-    B01000010,
-    B10100101,
-    B10000001,
-    B10111101,
-    B10000001,
-    B01000010,
-    B00111100 },
-  frown_bmp[] =
-  { B00111100,
-    B01000010,
-    B10100101,
-    B10000001,
-    B10011001,
-    B10100101,
-    B01000010,
-    B00111100 };
-*/
-
 void setup() {
 
-//  Serial.begin(9600);
-//  Serial.println("ISSI manual animation test");
-  if (! matrix.begin()) {
-  //  Serial.println("IS31 not found");
+  Serial.begin(9600);
+  Serial.println("ISSI manual animation test");
+  if (! matrix.begin()) 
+  {
+    Serial.println("IS31 not found");
     while (1);
   }
-//  Serial.println("IS31 Found!");
-  
+  Serial.println("IS31 Found!");
+
+  matrix.setRotation(0);
+  matrix.setCursor(0,0,);
+  matrix.setTextSize(1);
+  matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
+  matrix.setTextColor(100);  
 }
 
 void loop() 
 {
-  matrix.setRotation(0);
 
-  matrix.setTextSize(1);
-  matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
-  matrix.setTextColor(100);
   for (int8_t x=0;
             x>=-200;
 //            x>=-100;  // too short
