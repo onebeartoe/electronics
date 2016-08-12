ceeb8ecc70032d6a8b6243422d9aff0ed754d1f6
onebeartoe <onebeartoe@gmail.com>
2016-08-12 15:45:44 -0500

    The scrolling was added.

--- badges/adafruit-feather-wings/Alphanumeric_14_Segment/scrolling_text/scrolling_text_access_point/scrolling_text_access_point.ino
+++ badges/adafruit-feather-wings/Alphanumeric_14_Segment/scrolling_text/scrolling_text_access_point/scrolling_text_access_point.ino
@@ -40,6 +40,26 @@ WiFiServer server(80);
 
 String localhostIp;
 
+int indecies[] = {0,1,2,3};
+
+String message = "  Go Spurs Go   *   Timmy Rules!   * Bruce Bruce!!!    *";
+
+int messageLength = message.length();
+
+/**
+  * This array holds the current values of the 4 alphanumeric segments.
+  */
+char displaybuffer[] = {' ', ' ', ' ', ' '};
+
+unsigned long lightPreviousMillis = 0;
+
+unsigned long currentMillis;
+
+/**
+ * This is the delay in milliseconds for the scrolling text.
+ */
+long scrollDelay = 350;
+
 void setup() 
 {
     w = 3;
@@ -64,6 +84,17 @@ void setup()
 
 void loop() 
 {
+    currentMillis = millis();
+    
+    if(currentMillis - lightPreviousMillis >= scrollDelay) 
+    {
+        // save the last time you blinked the LED
+        lightPreviousMillis = currentMillis;
+
+        updateDisplay();
+    }     
+    
+    
   // Check if a client has connected
   WiFiClient client = server.available();
   if (!client) 
@@ -177,4 +208,48 @@ void initQuadAlpha()
     alpha4.writeDisplay();
    
     Serial.println("The Adafruit_AlphaNum4 setup is complete.");
+}
+
+void updateDisplay()
+{
+    // shift the index into the message for each segment 
+    indecies[0] += 1;
+    indecies[1] += 1;
+    indecies[2] += 1;
+    indecies[3] += 1;
+
+    if(indecies[0] == messageLength)
+    {
+        indecies[0] = 0;
+    }
+
+    if(indecies[1] == messageLength)
+    {
+        indecies[1] = 0;
+    }
+
+    if(indecies[2] == messageLength)
+    {
+        indecies[2] = 0;
+    }
+
+    if(indecies[3] == messageLength)
+    {
+        indecies[3] = 0;
+    }
+  
+  // scroll down display
+  displaybuffer[0] = message[indecies[0]];
+  displaybuffer[1] = message[indecies[1]];
+  displaybuffer[2] = message[indecies[2]];
+  displaybuffer[3] = message[indecies[3]];
+ 
+  // set every digit to the buffer
+  alpha4.writeDigitAscii(0, displaybuffer[0]);
+  alpha4.writeDigitAscii(1, displaybuffer[1]);
+  alpha4.writeDigitAscii(2, displaybuffer[2]);
+  alpha4.writeDigitAscii(3, displaybuffer[3]);
+ 
+  // write it out!
+  alpha4.writeDisplay();    
 }
