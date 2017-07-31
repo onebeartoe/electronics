5ac68841cdf070857905726ca8bb8ebf24dca64e
onebeartoe <onebeartoe@gmail.com>
2017-07-31 15:29:17 -0500

    Yay! The Trinket USB keyboard example code works.

---
+++ src/main/arduino/keyboard-shortcuts/keyboard_shortcuts_trinket_5v/keyboard_shortcuts_trinket_5v.ino
@@ -0,0 +1,58 @@
+
+/**
+ * I tried this but it didnto work on an installed library in the Arduino IDE.
+ *
+ *      https://learn.adafruit.com/trinket-usb-keyboard/overview
+ *
+ * To get the sketch to compile, this library was installed into the user Arduino folder
+ *
+ *      https://github.com/adafruit/Adafruit-Trinket-USB/tree/master/TrinketKeyboard
+ *
+ * TrinketKeyboard example
+ * For Trinket by Adafruit Industries
+*/
+
+#include "TrinketKeyboard.h"
+
+#define PIN_BUTTON_CAPITAL_A 0
+#define PIN_BUTTON_STRING    2
+
+void setup()
+{
+  // button pins as inputs
+  pinMode(PIN_BUTTON_CAPITAL_A, INPUT);
+  pinMode(PIN_BUTTON_STRING, INPUT);
+
+  // setting input pins to high means turning on internal pull-up resistors
+  digitalWrite(PIN_BUTTON_CAPITAL_A, HIGH);
+  digitalWrite(PIN_BUTTON_STRING, HIGH);
+  // remember, the buttons are active-low, they read LOW when they are not pressed
+
+  // start USB stuff
+  TrinketKeyboard.begin();
+}
+
+void loop()
+{
+  // the poll function must be called at least once every 10 ms
+  // or cause a keystroke
+  // if it is not, then the computer may think that the device
+  // has stopped working, and give errors
+  TrinketKeyboard.poll();
+
+  if (digitalRead(PIN_BUTTON_CAPITAL_A) == LOW)
+  {
+    // this should type a capital A
+    TrinketKeyboard.pressKey(KEYCODE_MOD_LEFT_SHIFT, KEYCODE_A);
+    // this releases the key (otherwise it is held down!)
+    TrinketKeyboard.pressKey(0, 0);
+
+    delay(1000);
+  }
+
+  if (digitalRead(PIN_BUTTON_STRING) == LOW)
+  {
+    // type out a string using the Print class
+    TrinketKeyboard.print("Hello World!");
+  }
+}
