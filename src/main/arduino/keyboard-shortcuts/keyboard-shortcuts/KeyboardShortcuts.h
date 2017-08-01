d2b0cae91829ba853bcafd28a6d3d56f919a952c
onebeartoe <onebeartoe@gmail.com>
2017-08-01 10:37:14 -0500

    This is preparation work to have an object oriented and non-object oriented version of the intial keyboard shortcut sketch.

---
+++ src/main/arduino/keyboard-shortcuts/keyboard-shortcuts/KeyboardShortcuts.h
@@ -0,0 +1,12 @@
+
+
+int keyboardShortcutssss = 3;
+
+uint8_t NO_MODIFIER = 0;
+
+uint8_t CTRL_ALT_MODIFIERS = KEYCODE_MOD_LEFT_SHIFT & KEYCODE_MOD_LEFT_ALT;
+
+class KeyboardShortcut
+{
+    
+}
--- src/main/arduino/keyboard-shortcuts/keyboard_shortcuts_trinket_5v_no_oop/keyboard_shortcuts_trinket_5v_no_oop.ino
+++ src/main/arduino/keyboard-shortcuts/keyboard_shortcuts_trinket_5v_no_oop/keyboard_shortcuts_trinket_5v_no_oop.ino
@@ -14,27 +14,6 @@
 
 #include <TrinketKeyboard.h>
 
-#include "relative-path-includes.h"
-
-//-------------------------------------------------
-// Arduino #include relative paths; pound include baby!
-//
-// https://arduino.stackexchange.com/a/9575/25692
-//
-// Define your project's root folder:
-#define PROJECT_ROOT C:\home\owner\versioning\github\electronics\src\main\arduino\keyboard-shortcuts\keyboard-shortcuts\
-
-//Define a relative-to-absolute macro 'transformation' :
-#define TO_STRING(s) #s
-#define ABSOLUTE_PATH(root, relative_path) TO_STRING(root\relative_path)
-#define RELATIVE_PATH(library) ABSOLUTE_PATH(PROJECT_ROOT, library)
-
-// Include your files using the relative path :
-#include RELATIVE_PATH(KeyboardShortcuts.h)
-//#include RELATIVE_PATH(another\file\relative\path.h)
-//-------------------------------------------------
-
-
 #define PIN_BUTTON_CAPITAL_A 0
 #define PIN_BUTTON_STRING    2
 
@@ -64,18 +43,10 @@ void loop()
   if (digitalRead(PIN_BUTTON_CAPITAL_A) == LOW)
   {
     // this should type a capital A
-    TrinketKeyboard.pressKey(KEYCODE_MOD_LEFT_SHIFT, KEYCODE_A);
+//    TrinketKeyboard.pressKey(KEYCODE_MOD_LEFT_SHIFT, KEYCODE_A);
 
-    // press 5 if it is setting
-    if(keyboardShortcutssss == 5)
-    {
-      TrinketKeyboard.pressKey(NO_MODIFIER, KEYCODE_5);
-    }
-    else
-    {
-      // alt + shift AO+ o
-      TrinketKeyboard.pressKey(KEYCODE_RIGHT_ALT, KEYCODE_RIGHT_SHIFT, KEYCODE_O);
-    }
+    // Press alt + shift + o
+    TrinketKeyboard.pressKey(KEYCODE_RIGHT_ALT, KEYCODE_RIGHT_SHIFT, KEYCODE_O);
 
     // this releases the key (otherwise it is held down!)
     TrinketKeyboard.pressKey(0, 0);
