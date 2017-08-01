345a82a05259e5dc447f04821da68ec3faef6ccd
onebeartoe <onebeartoe@gmail.com>
2017-08-01 11:16:38 -0500

    These are whitespace changes.

--- src/main/arduino/adafruit/feather-wings/ILI9341-tft/swiper/swiper_oo/swiper_oo.ino
+++ src/main/arduino/adafruit/feather-wings/ILI9341-tft/swiper/swiper_oo/swiper_oo.ino
@@ -1,18 +1,18 @@
 
 /**
- * 
- * This application kinda just has the drawing area from the Adafruit example 
+ *
+ * This application kinda just has the drawing area from the Adafruit example
  * and adds image loading when the user clicks the cyan square.
- * 
+ *
  * Return to drawing mode by clicing the blue square.
- * 
+ *
  * Author: Roberto Marquez
  *         https://www.youtube.com/onebeartoe/videos
  *         http://electronics.onebeartoe.org/
- * 
- * My C with classes (C++) was a little rusty, so I used the following as a 
+ *
+ * My C with classes (C++) was a little rusty, so I used the following as a
  * resource on how to create a class for reuse:
- *      https://learn.adafruit.com/multi-tasking-the-arduino-part-1/a-classy-solution 
+ *      https://learn.adafruit.com/multi-tasking-the-arduino-part-1/a-classy-solution
  *
  */
 
@@ -51,8 +51,8 @@ void setup(void)
     Serial.println("FeatherWing TFT");
 
     setupTft();
-    
-    // SD Card initialization 
+
+    // SD Card initialization
     yield();
 
     Serial.print("Initializing SD card...");
@@ -60,106 +60,106 @@ void setup(void)
       Serial.println("failed!");
     }
     Serial.println("OK!");
-    
+
     bmpDraw("red.bmp", 0, 0);
 }
 
-void loop() 
+void loop()
 {
-  // Retrieve a point  
+  // Retrieve a point
   TS_Point p = ts.getPoint();
-  
+
     if(debugStatements)
     {
         Serial.print("X = "); Serial.print(p.x);
         Serial.print("\tY = "); Serial.print(p.y);
-        Serial.print("\tPressure = "); Serial.println(p.z);          
+        Serial.print("\tPressure = "); Serial.println(p.z);
     }
 
   // Scale from ~0->4000 to tft.width using the calibration #'s
   p.x = map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
   p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
 
-  if (p.y < BOXSIZE) 
+  if (p.y < BOXSIZE)
   {
      oldcolor = currentcolor;
 
-     if (p.x < BOXSIZE) 
-     { 
-       currentcolor = ILI9341_RED; 
+     if (p.x < BOXSIZE)
+     {
+       currentcolor = ILI9341_RED;
        tft.drawRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-     } 
-     else if (p.x < BOXSIZE*2) 
+     }
+     else if (p.x < BOXSIZE*2)
      {
        currentcolor = ILI9341_YELLOW;
        tft.drawRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-     } 
-     else if (p.x < BOXSIZE*3) 
+     }
+     else if (p.x < BOXSIZE*3)
      {
        currentcolor = ILI9341_GREEN;
        tft.drawRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-     } 
-     else if (p.x < BOXSIZE*4) 
+     }
+     else if (p.x < BOXSIZE*4)
      {
        currentcolor = ILI9341_CYAN;
        tft.drawRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-       
+
        Serial.println("Switching modes to image browsing");
-       
+
        mode = MODE_IMAGE_BROWSING;
-     } 
-     else if (p.x < BOXSIZE*5) 
+     }
+     else if (p.x < BOXSIZE*5)
      {
        currentcolor = ILI9341_BLUE;
        tft.drawRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-       
+
        // change back to drawing mode
        mode = MODE_DRAWING;
-     } 
-     else if (p.x < BOXSIZE*6) 
+     }
+     else if (p.x < BOXSIZE*6)
      {
         // swipe debugging mode
-  
+
 /**
- * A rotation of 3 put test facing the same direction 
+ * A rotation of 3 put test facing the same direction
  * as the actual swipes, but caused other drawing coordinates issues.
- * 
+ *
  *      tft.setRotation(3);//21
- */         
-         
+ */
+
         mode = MODE_SWIPE_DEBUGGING;
         currentcolor = ILI9341_MAGENTA;
         tft.drawRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-       
+
         tft.fillRect(0,BOXSIZE, tft.width(), tft.height(), ILI9341_MAGENTA);
         tft.setCursor(BOXSIZE, BOXSIZE);
-        tft.setTextColor(ILI9341_WHITE);  
+        tft.setTextColor(ILI9341_WHITE);
         tft.setTextSize(1);
      }
 
-     if (oldcolor != currentcolor) 
+     if (oldcolor != currentcolor)
      {
-        if (oldcolor == ILI9341_RED) 
+        if (oldcolor == ILI9341_RED)
           tft.fillRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_RED);
-        if (oldcolor == ILI9341_YELLOW) 
+        if (oldcolor == ILI9341_YELLOW)
           tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_YELLOW);
-        if (oldcolor == ILI9341_GREEN) 
+        if (oldcolor == ILI9341_GREEN)
           tft.fillRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, ILI9341_GREEN);
-        if (oldcolor == ILI9341_CYAN) 
+        if (oldcolor == ILI9341_CYAN)
           tft.fillRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, ILI9341_CYAN);
-        if (oldcolor == ILI9341_BLUE) 
+        if (oldcolor == ILI9341_BLUE)
           tft.fillRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, ILI9341_BLUE);
-        if (oldcolor == ILI9341_MAGENTA) 
+        if (oldcolor == ILI9341_MAGENTA)
           tft.fillRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, ILI9341_MAGENTA);
      }
   }
 
-    // check if the touchscreen has a hit on the area that is to the 
+    // check if the touchscreen has a hit on the area that is to the
     // right of the color/menu selectors
-    if (((p.y-PENRADIUS) > 0) && ((p.y+PENRADIUS) < tft.height())) 
+    if (((p.y-PENRADIUS) > 0) && ((p.y+PENRADIUS) < tft.height()))
     {
         // the current point is in the 'drawing area'
-        
+
         if(mode == MODE_DRAWING)
         {
             tft.fillCircle(p.x, p.y, PENRADIUS, currentcolor);
@@ -167,7 +167,7 @@ void loop()
 
         // check for a swipe in the x direction
         SwipeDirection swiped = tftWing.xSwipeCheck(p.x, p.y);
-        
+
         if(swiped != SwipeDirection::NO_SWIPE && mode == MODE_IMAGE_BROWSING)
         {
             Serial.println("drawing in image mode");
@@ -181,15 +181,15 @@ void loop()
                 case UP:
                 {
                     char * direction = "up";
-                    
+
                     printSwipeInfo(direction);
-                    
+
                     break;
                 }
                 case DOWN:
                 {
                     char * direction = "down";
-                    
+
                     printSwipeInfo(direction);
 
                     break;
@@ -197,31 +197,31 @@ void loop()
                 case LEFT:
                 {
                     char * direction = "left";
-                    
+
                     printSwipeInfo(direction);
 
-                    break;                    
+                    break;
                 }
                 case RIGHT:
                 {
                     char * direction = "right";
-                    
+
                     printSwipeInfo(direction);
 
-                    break;                    
+                    break;
                 }
                 default:
                 {
                     Serial.println("wha? " + swiped);
 
-// the tft was blasted with this print statements                    
+// the tft was blasted with this print statements
 //                    tft.print("wha? -  ");
 //                    tft.print(swiped);
 //                    tft.println();
                 }
             }
-        }   
-    }    
+        }
+    }
 }
 
 void printSwipeInfo(char * direction)
