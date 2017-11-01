72932dc066fe8973aca2b4344d14106450c88c62
Roberto Marquez <onebeartoe@gmail.com>
2017-10-31 22:44:55 -0500

    A call to analogWrite was added to control the speed of the motor, I think.

--- microcontrollers/src/main/microcontroller/motors/drivers/TB6612/adafruit/2448/examples/pwm_motor_with_pro_trinket/pwm_motor_with_pro_trinket.ino
+++ microcontrollers/src/main/microcontroller/motors/drivers/TB6612/adafruit/2448/examples/pwm_motor_with_pro_trinket/pwm_motor_with_pro_trinket.ino
@@ -2,6 +2,7 @@
 // Setup the motor pins numbers.
 #define MOTOR_A_INPUT_1 4
 #define MOTOR_A_INPUT_2 8
+#define MOTOR_A_PWM A3
 
 // Set the modes for the motor pins.
 void setup()
@@ -10,8 +11,10 @@ void setup()
 
     pinMode(MOTOR_A_INPUT_1, OUTPUT);
     pinMode(MOTOR_A_INPUT_2, OUTPUT);
-}
 
+    // set the motor to a low speed
+    analogWrite(MOTOR_A_PWM, 135);
+}
 
 void loop()
 {
