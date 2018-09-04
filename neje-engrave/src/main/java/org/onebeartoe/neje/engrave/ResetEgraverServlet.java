d9a9e3b5233204b29baa35db839aaa2dd7ebcea8
Roberto Marquez <onebeartoe@gmail.com>
2018-09-04 14:19:37 -0500

    Loggers are now used, instead of System.out.println().  Commented code was removed.

--- neje-engrave/src/main/java/org/onebeartoe/neje/engrave/ResetEgraverServlet.java
+++ neje-engrave/src/main/java/org/onebeartoe/neje/engrave/ResetEgraverServlet.java
@@ -7,7 +7,6 @@ import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
