e53da365dd0b55482b702359c5c2d7f2f7e8dc77
Roberto Marquez <onebeartoe@gmail.com>
2018-09-04 16:22:51 -0500

    The code was cleaned up a little bit.

--- neje-engrave/src/main/java/org/onebeartoe/neje/engrave/filesystem/UploadServlet.java
+++ neje-engrave/src/main/java/org/onebeartoe/neje/engrave/filesystem/UploadServlet.java
@@ -2,7 +2,6 @@
 package org.onebeartoe.neje.engrave.filesystem;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
