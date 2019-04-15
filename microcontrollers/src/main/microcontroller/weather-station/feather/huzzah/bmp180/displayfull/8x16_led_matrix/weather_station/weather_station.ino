c0646523fc85d9a6cc716f6e274c8fc5b6b54423
Roberto Marquez <onebeartoe@gmail.com>
2019-04-15 18:52:01 -0500

    scroll a hardcoded string

--- microcontrollers/src/main/microcontroller/weather-station/feather/huzzah/bmp180/displayfull/8x16_led_matrix/weather_station/weather_station.ino
+++ microcontrollers/src/main/microcontroller/weather-station/feather/huzzah/bmp180/displayfull/8x16_led_matrix/weather_station/weather_station.ino
@@ -1,9 +1,10 @@
 
-// display: 
+// LED matrix display: 
+//
 //      https://www.adafruit.com/product/3150
 //
 //      https://learn.adafruit.com/adafruit-8x16-led-matrix-featherwing/overview
-
+//
 // temperature and humidity sensor
 //
 //      https://www.adafruit.com/product/1603
@@ -33,12 +34,25 @@ AdafruitIO_Feed *humidity = io.feed("rainmaker-backyard-top-pressure");
 unsigned long aioPostPreviousMillis = 0;
 const long aioPostInterval = 1000 * 60;  // once a minute
 
-// serial ouput interval settings
+// serial output interval settings
 unsigned long serialPreviousMillis = 0;
 const long serialInterval = 5000; // five seconds
 
+// LED display interval settings
+unsigned long ledDisplayPreviousMillis = 0;
+const long ledDisplayInterval = 100;  // one tenth of a second
+
 Adafruit_8x16minimatrix matrix = Adafruit_8x16minimatrix();
 
+// these next three hold values for the text scrolling
+int8_t x=7;
+int8_t xStart = 7;
+int8_t xMin = -36;
+
+/**
+ * Post the temperature and pressure values to 
+ * the Adafruit IO service.
+ */
 void aioPost(unsigned long currentMillis)
 {
     if(currentMillis - aioPostPreviousMillis >= aioPostInterval) 
@@ -54,48 +68,6 @@ void aioPost(unsigned long currentMillis)
         // save humidity to Adafruit IO
         humidity->save(pressureReading);
     }    
-}
-
-void setup() 
-{
-    // start the serial connection
-    Serial.begin(9600);
-
-    // wait for serial monitor to open
-    while(! Serial);
-
-    // initialize the temperature and pressure sensor
-    bmp.begin();
-
-    // connect to io.adafruit.com
-    Serial.println("\n\n");
-    Serial.println("\n\n");  
-    Serial.print("Connecting to Adafruit IO");
-    io.connect();
-
-    // wait for a connection
-    while(io.status() < AIO_CONNECTED) 
-    {
-      Serial.print(".");
-      delay(500);
-    }
-
-    // we are connected
-    Serial.println();
-    Serial.println(io.statusText());
-    
-    matrix.begin(0x70);  // pass in the address
-}
-
-void loop() 
-{
-    io.run();
-  
-    unsigned long currentMillis = millis();
-
-    serialOutput(currentMillis);
-    
-    aioPost(currentMillis);
 }
 
 void serialOutput(unsigned long currentMillis)
@@ -115,4 +87,83 @@ void serialOutput(unsigned long currentMillis)
         Serial.println(" Pa");
         Serial.println();    
     }
+}
+
+void setup() 
+{
+    // start the serial connection
+    Serial.begin(9600);
+
+    // wait for serial monitor to open
+    while(! Serial);
+
+    // initialize the temperature and pressure sensor
+    bmp.begin();
+
+    setupAdafruitIo();
+    
+    setupLedMatrix();
+}
+
+void setupAdafruitIo()
+{
+    // connect to io.adafruit.com
+    Serial.println("\n\n");
+    Serial.println("\n\n");  
+    Serial.print("Connecting to Adafruit IO");
+    io.connect();
+
+    // wait for a connection
+    while(io.status() < AIO_CONNECTED) 
+    {
+      Serial.print(".");
+      delay(500);
+    }
+
+    // we are connected
+    Serial.println();
+    Serial.println(io.statusText());    
+}
+
+void setupLedMatrix()
+{
+    matrix.begin(0x70);  // pass in the address    
+    
+    matrix.setTextSize(1);
+    matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
+    matrix.setTextColor(LED_ON);
+    matrix.setRotation(1);    
+}
+
+void ledDisplay(unsigned long currentMillis)
+{
+    if(currentMillis - ledDisplayPreviousMillis >= ledDisplayInterval) 
+    {
+        ledDisplayPreviousMillis = currentMillis;
+
+        matrix.clear();
+        matrix.setCursor(x,0);
+        matrix.print("World");
+        matrix.writeDisplay();
+        
+        x--;
+        
+        if(x == xMin)
+        {
+            x = xStart;
+        }
+    }
+}
+
+void loop() 
+{
+    io.run();
+  
+    unsigned long currentMillis = millis();
+
+    serialOutput(currentMillis);
+    
+    aioPost(currentMillis);
+    
+    ledDisplay(currentMillis);
 }
