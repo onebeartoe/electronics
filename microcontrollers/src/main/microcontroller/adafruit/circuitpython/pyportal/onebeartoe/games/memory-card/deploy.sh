47fdd4e3e5455894d1c143b6c463ec74239eb544
Roberto Marquez <onebeartoe@gmail.com>
2019-05-30 13:08:10 -0500

    show the name when the button is pressed

---
+++ microcontrollers/src/main/microcontroller/adafruit/circuitpython/pyportal/onebeartoe/games/memory-card/deploy.sh
@@ -0,0 +1,6 @@
+
+cd ~/Versioning/owner/github/onebeartoe/electronics/microcontrollers/src/main/microcontroller/adafruit/circuitpython/pyportal/onebeartoe/games/memory-card
+
+CIRCUITPY_PATH=/media/roberto/CIRCUITPY/
+
+cp main.py $CIRCUITPY_PATH
