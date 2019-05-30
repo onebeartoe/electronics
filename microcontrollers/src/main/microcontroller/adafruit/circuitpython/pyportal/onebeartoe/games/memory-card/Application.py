ab6c807d9b8120e043946d3d41228e42f4245d4b
Roberto Marquez <onebeartoe@gmail.com>
2019-05-30 18:50:53 -0500

    add an icon per button

--- microcontrollers/src/main/microcontroller/adafruit/circuitpython/pyportal/onebeartoe/games/memory-card/Application.py
+++ microcontrollers/src/main/microcontroller/adafruit/circuitpython/pyportal/onebeartoe/games/memory-card/Application.py
@@ -18,10 +18,6 @@ class Application(object):
         # Setup PyPortal without networking
         self.pyportal = PyPortal(default_bg=self.BACKGROUND_COLOR)
 
-        self.icon1Group = displayio.Group()
-        self.icon1Group.x = 0
-        self.icon1Group.y = 0
-
         self.initializeButtons()
 
         # Set the NeoPixel brightness
@@ -39,9 +35,13 @@ class Application(object):
         self.mode = 0
         self.mode_change = None
 
-
     def initializeButtons(self):
-		# Button colors
+        
+        self.icon1Group = displayio.Group()
+        self.icon1Group.x = 20
+        self.icon1Group.y = 20       
+        
+        # Button colors
         RED = (255, 0, 0)
         ORANGE = (255, 34, 0)
         YELLOW = (255, 170, 0)
@@ -55,38 +55,45 @@ class Application(object):
         WHITE = (255, 255, 255)
         OFF = (0, 0, 0)
 
-        snooze_file = open('/resources/images/cards/1.bmp', "rb")
-        icon = displayio.OnDiskBitmap(snooze_file)
-        try:
-            icon_sprite = displayio.TileGrid(icon,
-                                             pixel_shader=displayio.ColorConverter(),
-                                             x=0, y=0)
-        except TypeError:
-            icon_sprite = displayio.TileGrid(icon,
-                                             pixel_shader=displayio.ColorConverter(),
-                                             position=(0, 0))
-                                             
-        snooze_file.close()
-        self.icon1Group.append(icon_sprite)
-        self.pyportal.splash.append(self.icon1Group)
+        buttonPositions = [
+            {'pos1': (10, 10)},
+		    {'pos2': (90, 10)},
+            {'pos3': (170, 10)},
+		    {'pos4': (250, 10)},
+            {'pos5': (10, 90)},
+		    {'pos6': (90, 90)},
+		    {'pos7': (170, 90)},
+		    {'pos8': (250, 90)},
+		    {'pos9': (10, 170)},
+		    {'pos10': (90, 170)},
+		    {'pos11': (170, 170)},
+		    {'pos12': (250, 170)}
+            ]
 
         self.buttonAttributes = [
-        {'label': "1", 'pos': (10, 10),    'size': (60, 60), 'color': RED},
-		    {'label': "2", 'pos': (90, 10),    'size': (60, 60), 'color': ORANGE},
-		    {'label': "3", 'pos': (170, 10),   'size': (60, 60), 'color': YELLOW},
-		    {'label': "4", 'pos': (250, 10),   'size': (60, 60), 'color': GREEN},
-		    {'label': "5", 'pos': (10, 90),    'size': (60, 60), 'color': CYAN},
-		    {'label': "6", 'pos': (90, 90),    'size': (60, 60), 'color': BLUE},
-		    {'label': "7", 'pos': (170, 90),   'size': (60, 60), 'color': VIOLET},
-		    {'label': "8", 'pos': (250, 90),   'size': (60, 60), 'color': MAGENTA},
-		    {'label': "9", 'pos': (10, 170),   'size': (60, 60), 'color': PINK},
-		    {'label': "10", 'pos': (90, 170),  'size': (60, 60), 'color': AQUA},
+            {'label': "1", 'pos': (10, 10),    'size': (60, 60), 'color': WHITE},
+		    {'label': "2", 'pos': (90, 10),    'size': (60, 60), 'color': WHITE},
+            {'label': "3", 'pos': (170, 10),   'size': (60, 60), 'color': WHITE},
+		    {'label': "4", 'pos': (250, 10),   'size': (60, 60), 'color': WHITE},
+            {'label': "5", 'pos': (10, 90),    'size': (60, 60), 'color': WHITE},
+		    {'label': "6", 'pos': (90, 90),    'size': (60, 60), 'color': WHITE},
+		    {'label': "7", 'pos': (170, 90),   'size': (60, 60), 'color': WHITE},
+		    {'label': "8", 'pos': (250, 90),   'size': (60, 60), 'color': WHITE},
+		    {'label': "9", 'pos': (10, 170),   'size': (60, 60), 'color': WHITE},
+		    {'label': "10", 'pos': (90, 170),  'size': (60, 60), 'color': WHITE},
 		    {'label': "11", 'pos': (170, 170), 'size': (60, 60), 'color': WHITE},
-		    {'label': "12", 'pos': (250, 170), 'size': (60, 60), 'color': OFF}
-		    ]
+		    {'label': "12", 'pos': (250, 170), 'size': (60, 60), 'color': WHITE}
+            ]
 
         buttonFont = bitmap_font.load_font('/fonts/Arial-16.bdf')
         buttonFont.load_glyphs(b'0123456789CFabcdefghijklmnopqrstuvwxyz:')
+
+        snooze_file = open('/resources/images/cards/1.bmp', "rb")
+        icon = displayio.OnDiskBitmap(snooze_file)
+
+        self.icon_sprite = displayio.TileGrid(icon,
+                                         pixel_shader=displayio.ColorConverter(),
+                                         x=0, y=0)
 
         self.buttons = []
 
@@ -100,14 +107,24 @@ class Application(object):
                             outline_color=0x222222,
                             name=spot['label'],
                             label_font=buttonFont)
+            button.group.append(self.icon_sprite)
             self.pyportal.splash.append(button.group)
             self.buttons.append(button)
 
 
 
+
+# TODO: should we close the snooze_file?
+#        snooze_file.close()
+
+#        self.icon1Group.append(icon_sprite)
+        self.pyportal.splash.append(self.icon1Group)
+
+
+
     def launch(self):
-		# Calibrate light sensor on start to deal with different lighting situations
-		# If the self.mode change isn't responding properly, reset your PyPortal to recalibrate
+        # Calibrate light sensor on start to deal with different lighting situations
+        # If the self.mode change isn't responding properly, reset your PyPortal to recalibrate
         initial_light_value = self.light_sensor.value
         while True:
             if self.light_sensor.value < (initial_light_value * 0.3) and self.mode_change is None:
@@ -125,6 +142,7 @@ class Application(object):
                         spotIndex = int(button.name) - 1
                         button.label = self.buttonAttributes[spotIndex]['label']
                         self.pyportal.play_file('Coin.wav')
+                        self.icon1Group.append(self.icon_sprite)
                         print("Touched", button.name)
                         if self.mode == 0:
                             self.strip_1.fill(button.fill_color)
