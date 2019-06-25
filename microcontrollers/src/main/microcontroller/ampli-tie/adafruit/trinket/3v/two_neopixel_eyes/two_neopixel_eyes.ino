8844e3857e09a1d2fbdb65a3fe48a35d93eaccc4
Roberto Marquez <onebeartoe@gmail.com>
2019-06-25 11:14:16 -0500

    add more example code

---
+++ microcontrollers/src/main/microcontroller/ampli-tie/adafruit/trinket/3v/two_neopixel_eyes/two_neopixel_eyes.ino
@@ -0,0 +1,51 @@
+
+#include <Adafruit_NeoPixel.h>
+
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+#define N_PIXELS  3  // Number of pixels in strand
+
+#define LED_PIN    4  // NeoPixel LED strand is connected to this pin
+
+
+Adafruit_NeoPixel
+  strip = Adafruit_NeoPixel(N_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() 
+{
+  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
+  // Any other board, you can remove this part (but no harm leaving it):
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  // END of Trinket-specific code.    
+    
+    strip.begin();
+    strip.show();
+    strip.setBrightness(24);
+  
+}
+
+void loop()
+{
+    theaterChase(strip.Color(  0,   0, 127), 50);
+}
+
+// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
+// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
+// between frames.
+void theaterChase(uint32_t color, int wait) {
+  for(int a=0; a<10; a++) {  // Repeat 10 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in steps of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show(); // Update strip with new contents
+      delay(wait);  // Pause for a moment
+    }
+  }
+}
