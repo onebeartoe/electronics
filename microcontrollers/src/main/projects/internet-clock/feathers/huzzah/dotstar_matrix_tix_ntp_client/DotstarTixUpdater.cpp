c47032dbc79c69d0b072e9910967a204b7643a6e
Roberto Marquez <onebeartoe@gmail.com>
2019-08-10 00:33:10 -0500

    baseline

---
+++ microcontrollers/src/main/projects/internet-clock/feathers/huzzah/dotstar_matrix_tix_ntp_client/DotstarTixUpdater.cpp
@@ -0,0 +1,80 @@
+
+#ifndef onebeartoe_DotstarTimeUpdater_cpp
+#define onebeartoe_DotstarTimeUpdater_cpp
+
+#include <Adafruit_DotStarMatrix.h>
+
+#include "DotstarTixUpdater.h"
+
+#include "/home/roberto/Versioning/owner/github/onebeartoe/electronics/microcontrollers/src/main/projects/badges/adafruit-feather-wings/alphanumeric_14_segment/src/TextWingModes.h"
+
+#include "/home/roberto/Versioning/owner/github/onebeartoe/electronics/microcontrollers/src/main/projects/internet-clock/src/InternetClock.h"
+//#include "/home/roberto/Versioning/owner/github/onebeartoe/electronics/microcontrollers/src/main/projects/internet-clock/src/InternetClock.cpp"
+
+#include "/home/roberto/Versioning/owner/github/onebeartoe/electronics/microcontrollers/src/main/microcontroller/adafruit/feather-wings/dotstar-matrix/src/DotstarMatrixWing.h"
+//#include "/home/roberto/Versioning/owner/github/onebeartoe/electronics/microcontrollers/src/main/microcontroller/adafruit/feather-wings/dotstar-matrix/src/DotstarMatrixWing.cpp"
+
+DotstarTixUpdater::DotstarTixUpdater(InternetClock* clock, DotstarMatrixWing* dotstarWing)
+{
+    interval = 20 * 1000;
+    
+    this->dotstarWing = dotstarWing;
+    
+    this->dotstarWing->dotstarMatrix->setTextSize(0.5);
+    
+    this->clock = clock;
+}
+
+void DotstarTixUpdater::oneLoop()
+{
+    char str[50] = "";
+    
+    if(previousMinute != clock->minute)
+    {
+        sprintf(str, "%2d%2d", clock->hour, clock->minute);
+    
+        Serial.println();
+        Serial.print("updating the display with this time: >");
+        Serial.print(str);
+        Serial.println("<");
+
+//        dotstarWing->setText(str);
+//        dotstarWing->setTextMode(SCROLLING_TEXT);
+
+//        scrollTime();
+
+        dotstarWing->setTextMode(STILL);
+        dotstarWing->setText(str);    
+
+        dotstarWing->dotstarMatrix->setCursor(0, 5);
+        dotstarWing->dotstarMatrix->print(str);
+        dotstarWing->dotstarMatrix->show();
+        
+        // reset the minute
+        previousMinute = clock->minute;
+    }
+}
+
+void DotstarTixUpdater::scrollTime()
+{
+    unsigned long currentMillis = millis();
+    
+    unsigned long startTime = millis();
+     
+    long scrollInterval = 3 * 1000; // this is in milliseconds
+    
+    Serial.println();
+    Serial.print("scroll time: ");
+    Serial.print(dotstarWing->text);    
+
+    while(currentMillis < scrollInterval + startTime) 
+    {
+        currentMillis = millis();
+
+        dotstarWing->loop();
+    }
+    
+    Serial.println(" - end scroll");
+}
+
+#endif
---
+++ microcontrollers/src/main/projects/internet-clock/feathers/huzzah/dotstar_matrix_tix_ntp_client/dotstar_matrix_tix_ntp_client.ino
@@ -0,0 +1,41 @@
+
+#include <Adafruit_DotStarMatrix.h>
+
+#include "DotstarTixUpdater.h"
+
+#include "/home/roberto/Versioning/owner/github/onebeartoe/electronics/microcontrollers/src/main/projects/internet-clock/src/InternetClock.h"
+#include "/home/roberto/Versioning/owner/github/onebeartoe/electronics/microcontrollers/src/main/projects/internet-clock/src/InternetClock.cpp"
+
+#include "/home/roberto/Versioning/owner/github/onebeartoe/electronics/microcontrollers/src/main/microcontroller/adafruit/feather-wings/dotstar-matrix/src/DotstarMatrixWing.h"
+#include "/home/roberto/Versioning/owner/github/onebeartoe/electronics/microcontrollers/src/main/microcontroller/adafruit/feather-wings/dotstar-matrix/src/DotstarMatrixWing.cpp"
+
+#include "/home/roberto/Versioning/owner/github/onebeartoe/electronics/microcontrollers/src/main/microcontroller/adafruit/feather-wings/dotstar-matrix/src/TextDisplableWing.h"
+#include "/home/roberto/Versioning/owner/github/onebeartoe/electronics/microcontrollers/src/main/microcontroller/adafruit/feather-wings/dotstar-matrix/src/TextDisplableWing.cpp"
+
+Adafruit_DotStarMatrix matrix = Adafruit_DotStarMatrix(12, 6, 
+                                                       DATAPIN, CLOCKPIN,
+                                                       DS_MATRIX_BOTTOM + DS_MATRIX_LEFT + DS_MATRIX_ROWS + DS_MATRIX_PROGRESSIVE,
+                                                       DOTSTAR_BGR);
+
+InternetClock clock;
+
+DotstarMatrixWing dotstarWing(&matrix);
+
+DotstarTixUpdater displayUpdater(&clock, &dotstarWing);
+
+void setup() 
+{
+    Serial.begin(9600);
+
+    Serial.println("dotstar matrix wing ntp client");
+    
+    // set a smaller spread than the default since there text string is short (only 4 chars)
+    dotstarWing.xSpread = 20;
+}
+
+void loop()
+{
+    clock.loop();
+    
+    displayUpdater.loop();
+}
