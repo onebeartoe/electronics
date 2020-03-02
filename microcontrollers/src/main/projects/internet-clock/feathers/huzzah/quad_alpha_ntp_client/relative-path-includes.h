8b8366fbbb54715d4b6e59f14f343a3d18e898be
Roberto Marquez <onebeartoe@gmail.com>
2020-03-02 11:47:20 -0600

    cleanup

--- microcontrollers/src/main/projects/internet-clock/feathers/huzzah/quad_alpha_ntp_client/relative-path-includes.h
+++ microcontrollers/src/main/projects/internet-clock/feathers/huzzah/quad_alpha_ntp_client/relative-path-includes.h
@@ -1,6 +1,5 @@
 
 #define PROJECT_ROOT /home/roberto/Versioning/owner/github/onebeartoe/electronics/microcontrollers/src/main
-//#define PROJECT_ROOT /home/roberto/Versioning/owner/github/onebeartoe/electronics/microcontrollers/src/main/microcontroller
 
 #include "/home/roberto/Versioning/owner/github/onebeartoe/electronics/microcontrollers/src/main/microcontroller/relative-path-includes.cpp"
 
@@ -14,3 +13,4 @@
 
 #include RELATIVE_PATH(projects/badges/adafruit-feather-wings/alphanumeric_14_segment/src/QuadAlphanumericWing.h)
 #include RELATIVE_PATH(projects/badges/adafruit-feather-wings/alphanumeric_14_segment/src/QuadAlphanumericWing.cpp)
+
