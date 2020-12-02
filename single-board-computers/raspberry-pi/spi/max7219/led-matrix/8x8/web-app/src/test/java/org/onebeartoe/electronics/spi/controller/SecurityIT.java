1e4baf11b7f97d08ae7055a571c91a415af81b9d
Roberto Marquez <onebeartoe@gmail.com>
2020-12-02 09:46:16 -0700

    cleanup

--- single-board-computers/raspberry-pi/spi/max7219/led-matrix/8x8/web-app/src/test/java/org/onebeartoe/electronics/spi/controller/SecurityIT.java
+++ single-board-computers/raspberry-pi/spi/max7219/led-matrix/8x8/web-app/src/test/java/org/onebeartoe/electronics/spi/controller/SecurityIT.java
@@ -4,17 +4,12 @@ package org.onebeartoe.electronics.spi.controller;
 import java.net.URI;
 import java.net.URL;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.onebeartoe.electronics.spi.rest.ModeControlsIT.userName;
-import static org.onebeartoe.electronics.spi.rest.ModeControlsIT.userPassword;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
-
 
 /**
  * AC5
