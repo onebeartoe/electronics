688897141bd3306ec966704290c3e8dae4ec302f
Roberto Marquez <onebeartoe@gmail.com>
2021-07-07 12:13:43 -0600

    whitespace

--- single-board-computers/raspberry-pi/spi/max7219/led-matrix/8x8/web-app/src/main/java/org/onebeartoe/electronics/spi/controller/ControlsController.java
+++ single-board-computers/raspberry-pi/spi/max7219/led-matrix/8x8/web-app/src/main/java/org/onebeartoe/electronics/spi/controller/ControlsController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 /**
  * This class sends an HTML UI for the products listing page.
  */
