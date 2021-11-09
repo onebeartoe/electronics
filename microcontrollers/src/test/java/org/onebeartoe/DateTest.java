80dbbc5268536bb5bb88c08dd5c76a43553cad84
Roberto Marquez <onebeartoe@gmail.com>
2021-11-09 11:53:47 -0700

    cleanup and add a todo

--- microcontrollers/src/test/java/org/onebeartoe/DateTest.java
+++ microcontrollers/src/test/java/org/onebeartoe/DateTest.java
@@ -1,23 +1,17 @@
-/*
- */
+
 package org.onebeartoe;
 
 import java.util.Date;
-import org.testng.annotations.Test;
 
 /**
  *
- * @author Roberto Marquez
  */
 public class DateTest
 {
-    @Test
+  
+    //TODO: delete this 
     public void testDate()
     {
-        Date d = new Date();
         
-        int date = d.getDate();
-                
-        System.out.println("date: " + date);
     }
 }
